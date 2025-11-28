98c89a9e5c67b8af4af319b3358835d42fae5787
shariful4051 <sorif19219004051@gmail.com>
2025-11-28 14:39:54 +0000

    server set up done

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port =process.env.PORT|| 3000
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('study mate server is running')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
