6941951534bb57ff747f3e06d8e6d34ffb866c2b
shariful4051 <sorif19219004051@gmail.com>
2025-11-29 16:22:12 +0000

    find partners top 3 partners my connection api and .env file done

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors')
+require('dotenv').config()
 const app = express()
 const port =process.env.PORT|| 3000
 app.use(cors())
 app.use(express.json())
 
-const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.tn7v7f9.mongodb.net/?appName=Cluster0";
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tn7v7f9.mongodb.net/?appName=Cluster0`;
 
 
 const client = new MongoClient(uri, {
@@ -25,6 +29,76 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const db = client.db('studyMate')
+    const allpartnersCollection = db.collection('allpartners')
+    const myConnectionsCollection = db.collection('myconnection')
+
+//---------------all partners api-----------
+    app.get('/allpartners',async(req,res)=>{
+        const cursor = allpartnersCollection.find()
+        const result = await cursor.toArray()
+        res.send(result)
+    })
+    
+    app.get('/allpartners/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id:new ObjectId(id)}
+      const result = await allpartnersCollection.findOne(query);
+      res.send(result)
+    })
+
+    app.get('/highRating',async(req,res)=>{
+      const cursor= allpartnersCollection.find().sort({rating:-1}).limit(3)
+      const result = await cursor.toArray()
+        res.send(result)
+      
+    })
+    app.post('/allpartners',async(req,res)=>{
+      const profile = req.body;
+      const result = await allpartnersCollection.insertOne(profile)
+      res.send(result)
+    })
+
+   //----------- myconnection api---------
+
+   app.get('/myconnection',async(req,res)=>{
+    const email = req.query.email;
+    const query={}
+    if(email){
+      query.myEmail=email;
+    }
+    const cursor = myConnectionsCollection.find(query)
+    const result = await cursor.toArray()
+    res.send(result)
+   })
+
+   app.post('/myconnection',async(req,res)=>{
+      const partner = req.body;
+      const result = await myConnectionsCollection.insertOne(partner)
+      res.send(result)
+    })
+
+    app.delete('/myconnection/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id:new ObjectId(id)}
+      const result = await myConnectionsCollection.deleteOne(query)
+      res.send(result)
+    })
+
+    app.patch('/myconnection/:id',async(req,res)=>{
+      const id = req.params.id;
+      const data = req.body;
+      console.log('from pudate',data,id);
+      const query = {_id:new ObjectId(id)}
+      const update = {
+        $set:data
+        
+      }
+      const result = await myConnectionsCollection.updateOne(query,update)
+      res.send(result)
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
